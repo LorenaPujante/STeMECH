510af8dbdc6b2445688d1732b78a60935f2cac01
Lorena Pujante Otálora <154461660+LorenaPujante@users.noreply.github.com>
2024-10-30 17:46:43 +0100

    Update trajectoryDist_DTW_SpTemp.py

--- Code/trajectoryDist_DTW_SpTemp.py
+++ Code/trajectoryDist_DTW_SpTemp.py
@@ -3,9 +3,11 @@ from config import *
 
 def dtw_SpTemp(stepsP1, stepsP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa):
 
-    # Caso base     # Se reduce a uno solo
+    # Base cases    
+    if len(stepsP1)==0 and len(stepsP2)==0:
+        return [0,0]    
     if len(stepsP1)==0 or len(stepsP2)==0:
-        return 0    # Nada de similitud
+        return [999999,0]
     
     
 
@@ -19,6 +21,8 @@ def dtw_SpTemp(stepsP1, stepsP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)
     headP2 = stepsP2[0]
     restP1 = stepsP1[1:]
     restP2 = stepsP2[1:]
+    ev1 = headP1[1]
+    ev2 = headP2[1]
     step1 = headP1[0]
     step2 = headP2[0]
     
@@ -28,15 +32,28 @@ def dtw_SpTemp(stepsP1, stepsP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)
 
 
     sim_HeadP1_HeadP2 = getSimilarity(headP1, headP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)
-    
-    sim_P1_RestP2 = dtw_SpTemp(stepsP1, restP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)     # Option 1
-    sim_RestP1_P2 = dtw_SpTemp(restP1, stepsP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)     # Option 2
-    sim_RestP1_RestP2 = dtw_SpTemp(restP1, restP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)  # Option 3
-    listSimilarity = [sim_P1_RestP2, sim_RestP1_P2, sim_RestP1_RestP2]
-    maxSimilarity = max(listSimilarity)
+    dist_HeadP1_HeadP2_sp = getSpatialDistance(ev1, ev2, sp_matrix, hu_matrix)
+    dist_HeadP1_HeadP2_tmp = getTemporalDistance(step1, step2, maxDiffSteps)*(1-beta)
+    dist_HeadP1_HeadP2 =  (alfa*dist_HeadP1_HeadP2_sp) + ((1-alfa)*dist_HeadP1_HeadP2_tmp)
 
-    similarity = sim_HeadP1_HeadP2 + maxSimilarity
-    # Optimizacion
-    matrixes_opt['matrixDTW_SP'][step1][step2] = similarity
+    res_P1_RestP2 = dtw_SpTemp(stepsP1, restP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)     # Option 1
+    res_RestP1_P2 = dtw_SpTemp(restP1, stepsP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)     # Option 2
+    res_RestP1_RestP2 = dtw_SpTemp(restP1, restP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa)  # Option 3
     
-    return similarity
+    listResults = [res_P1_RestP2, res_RestP1_P2, res_RestP1_RestP2]
+    minDist = 9999999999
+    maxSim = 0
+    for r in listResults:
+        if r[0]<minDist:
+            minDist = r[0]
+            maxSim = r[1]
+        elif r[0]==minDist:
+            if r[1] > maxSim:
+                maxSim = r[1]
+    distance = dist_HeadP1_HeadP2 + minDist
+    similarity = sim_HeadP1_HeadP2 + maxSim
+    
+    # Optimizacion
+    matrixes_opt['matrixDTW_SP'][step1][step2] = [distance, similarity]
+    
+    return [distance, similarity]
