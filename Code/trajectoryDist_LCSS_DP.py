b0fc88354ed832d8e74805f3796689b87adde71e
Lorena Pujante Otálora <154461660+LorenaPujante@users.noreply.github.com>
2024-11-05 12:49:11 +0100

    Update trajectoryDist_LCSS_DP.py

--- Code/trajectoryDist_LCSS_DP.py
+++ Code/trajectoryDist_LCSS_DP.py
@@ -10,7 +10,7 @@ from config import *
 
 def lcss_dp(stepsP1, stepsP2, sp_matrix, hu_matrix, maxDiffSteps, beta, alfa):
     
-    minSpatialSim = getSpatialSimilarity_distKnown(39.5/102.5)         # DEFINICION DE BROTE   # Distancia hasta Area -> Zona Lógica           # 39.5 = Distancia Bed-LZ-Bed   # 102.5 = Valor máximo en la Matriz de Locations
+    minSpatialSim = getSpatialSimilarity_distKnown(required_parameters['maxSpDist'])
     maxTempDist = getTemporalDistance_diffKnown(required_parameters['maxStepsBackwardLCSS'], maxDiffSteps)
     minTemporalSim = getTemporalSimilarity_distKnown(maxTempDist, beta)
                                 
